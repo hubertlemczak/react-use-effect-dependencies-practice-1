046ebe87e6542c583ab66f078b64abab7e5cc23f
Hubert Lemczak <hubertlemczak@gmail.com>
2022-07-22 11:40:27 +0100

    complete

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import { useEffect, useState } from "react";
-import DataList from "./components/DataList";
-import SelectTypeForm from "./components/SelectTypeForm";
-import "./styles.css";
+import { useEffect, useState } from 'react';
+import DataList from './components/DataList';
+import SelectTypeForm from './components/SelectTypeForm';
+import './styles.css';
 
 export default function App() {
-  const [dataType, setDataType] = useState("");
-
+  const [dataType, setDataType] = useState('');
   const [data, setData] = useState(null);
 
-  console.log({ data });
-
-  // Write code here.
-  //
+  useEffect(() => {
+    if (dataType) {
+      const getData = async () => {
+        console.log('fetching', dataType);
+        const res = await fetch(`https://swapi.dev/api/${dataType}/`);
+        const resData = await res.json();
+        console.log('resData', resData);
+        setData(resData);
+      };
+      getData();
+    }
+  }, [dataType]);
 
   return (
     <div>
-      <SelectTypeForm setDataType={setDataType} />
+      <SelectTypeForm setDataType={setDataType} dataType={dataType} />
       {data && <DataList dataType={dataType} data={data.results} />}
     </div>
   );
